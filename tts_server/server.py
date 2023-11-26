acba9dadefbf378786462811b1f87b2e9c591280
Aleksey Smolenchuk <lxe@lxe.co>
2023-11-26 15:26:21 -0800

    Moved styletts2 to lxe/styletts2 importable

--- tts_server/server.py
+++ tts_server/server.py
@@ -3,7 +3,8 @@ import os
 import time
 import soundfile as sf
 import argparse
-from . import tts # import here to avoid loading it when running --help
+# import langdetect
+from styletts2 import TTS  # import here to avoid loading it when running --help
 
 app = Flask(__name__)
 cwd = os.getcwd()
@@ -11,11 +12,15 @@ fdir = os.path.dirname(__file__)
 
 # Cache for styles
 styles = {}
-s_prevs = {}
+prev_s_db = {}
+
+tts = None
+
 
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({'error': 'Not found'}), 404
+
 
 @app.route('/destroy_session', methods=['DELETE'])
 def destroy_session():
@@ -29,6 +34,7 @@ def destroy_session():
         os.system(f'rm -rf {session_dir}')
 
     return jsonify({"success": True}), 200
+
 
 @app.route('/download', methods=['GET'])
 def download_file():
@@ -46,21 +52,26 @@ def download_file():
 
     return send_from_directory(session_dir, filename, as_attachment=True)
 
+
 @app.route('/tts', methods=['POST'])
 def text_to_speech():
     try:
         data = request.json
-        
+
         session = data.get('session')
         text = data.get('text')
-        
+
         # Check if session and text are provided
         if not session:
             return jsonify({"error": "Missing session"}), 400
         if not text:
             return jsonify({"error": "Missing text"}), 400
-        
-        voice = data.get('style', 'en-f-1')    
+
+        # language = data.get('language', None)
+        # if language is None:
+        #     language = langdetect.detect(text)
+
+        voice = data.get('style', 'en-f-1')
         alpha = data.get('alpha', 0.1)
         beta = data.get('beta', 0.3)
         diffusion_steps = data.get('diffusion_steps', 5)
@@ -75,32 +86,32 @@ def text_to_speech():
         if voice not in styles:
             voice_fname = os.path.join(fdir, 'voices', f'{voice}.wav')
             styles[voice] = tts.compute_style(voice_fname)
- 
+
         style = styles[voice]
-        
+
         # Generate key for previous voice state
         vkey = f"{session}/{voice}"
-        s_prev = s_prevs.get(vkey, None)
+        prev_s = prev_s_db.get(vkey, None)
 
         # Generate audio
-        wav, s_prev = tts.LFinference(
-            text, 
-            s_prev,
-            style, 
-            alpha=alpha, 
-            beta=beta, 
-            diffusion_steps=diffusion_steps, 
+        wav, prev_s = tts.inference(
+            text,
+            style,
+            prev_s=prev_s,
+            alpha=alpha,
+            beta=beta,
+            diffusion_steps=diffusion_steps,
             embedding_scale=embedding_scale
         )
-        
+
         # Update previous voice state
-        s_prevs[vkey] = s_prev
+        prev_s_db[vkey] = prev_s
 
         # Save audio
         filename = f"{int(time.time())}.wav"
         filepath = os.path.join(session_dir, filename)
         sf.write(filepath, wav, 24000)
-        
+
         # If we are accepting wav's, return the wav
         if request.headers.get('Accept') == 'audio/wav':
             return send_from_directory(session_dir, filename, as_attachment=True)
@@ -109,18 +120,29 @@ def text_to_speech():
     except Exception as e:
         raise
         return jsonify({"error": str(e)}), 500
-        
-def main():
-    parser = argparse.ArgumentParser(description='Run the Text-to-Speech Server')
 
-    parser.add_argument('--debug', action='store_true', help='Enable debug mode')
-    parser.add_argument('--host', default='127.0.0.1', help='Host IP address (default: 127.0.0.1)')
-    parser.add_argument('--port', type=int, default=5050, help='Port number (default: 5050)')
+
+def main():
+    global tts
+    parser = argparse.ArgumentParser(
+        description='Run the Text-to-Speech Server')
+
+    parser.add_argument('--debug', action='store_true',
+                        help='Enable debug mode')
+    parser.add_argument('--host', default='127.0.0.1',
+                        help='Host IP address (default: 127.0.0.1)')
+    parser.add_argument('--port', type=int, default=5050,
+                        help='Port number (default: 5050)')
 
     args = parser.parse_args()
-    
+
+    tts = TTS.load_model(
+        config_path="hf://yl4579/StyleTTS2-LibriTTS/Models/LibriTTS/config.yml",
+        checkpoint_path="hf://yl4579/StyleTTS2-LibriTTS/Models/LibriTTS/epochs_2nd_00020.pth"
+    )
 
     app.run(debug=args.debug, host=args.host, port=args.port)
-    
+
+
 if __name__ == '__main__':
     main()
