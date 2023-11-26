a94bd51af00f3a16e96ee502a88af299a40dfa2c
Aleksey Smolenchuk <lxe@lxe.co>
2023-11-26 15:39:35 -0800

    Fixed packaging

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="tts_server",
     version="1.0.0",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "flask",
         "styletts2 @ git+https://github.com/lxe/styletts2.git",
